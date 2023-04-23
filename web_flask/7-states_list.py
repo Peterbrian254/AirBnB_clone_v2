e6ed8e36e43760434db061de5084d5f0cb69d109
Peterbrian254 <peterbrian8957@gmail.com>
2023-04-23 09:16:15 +0000

    new

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application: List of states
-The web application must be listening on 0.0.0.0, port 5000"""
+"""Minimal flask app"""
+
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-
-
+from models import State
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def display_states():
-    """Render state_list html page to display States created"""
-    states = storage.all()
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown(self):
-    """Method to remove current SQLAlchemy Session"""
+def closedb(foo):
+    """Closes db session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Route /states_list"""
+    states = list(storage.all(State).values())
+    states.sort(key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    storage.reload()
+    app.run("0.0.0.0", 5000)
